a0a348a8d732eee4082565e4a1045cb1c75cfc18
felixNyalenda <felixasejunior@gmail.com>
2023-04-10 17:06:37 +0300

    work-experience build

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,7 +16,7 @@ const Hero = () => {
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className="text-[#915eff]">Felix Olali</span>
+            Hi, I'm <span className="text-[#915eff]">Felix Nyalenda</span>
           </h1>
 
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
