4db65c4e74a5eb34a004fcdcb9ef66445ac49a0f
felixNyalenda <felixasejunior@gmail.com>
2023-04-10 18:14:10 +0300

    technology build

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import SectionWrapper from "../hoc/SectionWrapper";
+import BallCanvas from "./canvas/Ball";
+import { technologies } from "../constants/index";
 
 const Tech = () => {
   return (
-    <div>Tech</div>
-  )
-}
+    <div className="flex flex-row flex-wrap justify-center gap-10">
+      {technologies.map(({ name, icon }) => (
+        <div key={name} className="h-28 w-28">
+          <BallCanvas icon={icon} />
+        </div>
+      ))}
+    </div>
+  );
+};
 
-export default Tech
+export default SectionWrapper(Tech, "");
